0ba189b400aa8e16ffc9acb71ac6fc1d84a11a26
40204649 <40204649@CPC402-4649>
2018-09-14 15:14:33 +0800

    XmlToHtml

---
+++ XmlToHtmlByXslt/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.Xsl;
+
+namespace XmlToHtmlByXslt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xslCompiledTransform = new XslCompiledTransform();
+            xslCompiledTransform.Load("sample.xslt");
+
+            var htmlOutput = new StringBuilder();
+            var htmlWriter = new StringWriter(htmlOutput);
+
+            // Creating XmlReader object to read XML content    
+            using (var reader = XmlReader.Create("sample.xml"))
+            {
+                // Call Transform() method to create html string and write in TextWriter object.    
+                xslCompiledTransform.Transform(reader, null, htmlWriter);
+            }
+
+            Console.WriteLine(htmlOutput.ToString());
+        }
+    }
+}
