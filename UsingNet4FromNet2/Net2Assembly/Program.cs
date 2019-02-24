6f839d55607419ed18af3142949a1f75dc38c8a3
Hon Loon <tenghonloon@hotmail.com>
2019-02-24 16:18:37 +0800

    UsingNet4FromNet2

---
+++ UsingNet4FromNet2/Net2Assembly/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Net4ToNet2Adapter;
+
+namespace Net2Assembly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("CLR version from EXE: {0}", Environment.Version);
+
+            var myClassAdapterType = Type.GetTypeFromProgID("Net4ToNet2Adapter.MyClassAdapter");
+            var myClassAdapterInstance = Activator.CreateInstance(myClassAdapterType);
+            var myClassAdapter = (IMyClassAdapter)myClassAdapterInstance;
+            myClassAdapter.DoNet4Action();
+        }
+    }
+}
